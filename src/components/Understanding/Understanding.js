ade6d88f18e155983a9db6cd171bc1475799c475
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-01 20:18:09 -0500

    add understanding form

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -5,8 +5,21 @@ class Understanding extends Component {
   render() {
     return (
       <div >
-       <h1>How well are you understanding the content?</h1>
+        <h1>How well are you understanding the content?</h1>
+        Understanding?
+       <form onSubmit={this.handleUnderstanding}>
+          <select name="feelings">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <br /><br />
+          <button>Next</button>
+        </form>
       </div>
+      </div >
     );
   }
 }
