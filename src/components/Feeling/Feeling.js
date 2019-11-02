4c75fac5ea1d3198ac819e6f55c48c3bd9b981f6
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-01 19:35:14 -0500

    add inputs and form to feelings

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -4,8 +4,19 @@ import axios from 'axios';
 class Feeling extends Component {
   render() {
     return (
-      <div >
+      <div>
        <h1>How are you feeling today?</h1>
+       <form action="/understanding">
+  <select name="feelings">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select>
+  <br /><br />
+  <input type="submit" />
+</form>
       </div>
     );
   }
